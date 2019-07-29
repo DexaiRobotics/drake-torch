00b74b4b871b4e85149a4d57375be5c9dd94f140
awang27 <44820652+awang27@users.noreply.github.com>
2019-07-29 11:47:30 -0400

    python3

--- scripts/fix_bashrc.sh
+++ scripts/fix_bashrc.sh
@@ -26,4 +26,5 @@ elif [[ -f /opt/ros/$ROS_DISTRO/setup.bash ]]; then
     echo "found /opt/ros/$ROS_DISTRO/setup.bash. sourcing..."
     source /opt/ros/$ROS_DISTRO/setup.bash
 fi
+export ROS_PYTHON_VERSION=3
 EOF
