4d5fd88d7e7a685dc656d0fc29b2d4dec5021253
Duan Yutong <duan.yutong@dexai.com>
2020-06-07 14:10:57 +0000

    typo

--- scripts/setup_pydrake.py
+++ scripts/setup_pydrake.py
@@ -1,6 +1,6 @@
 """Setup script for drake to enable import without modifying pythonpath.
 
-Install in editable/development mode with 'pip install .'
+Install in editable/development mode with 'pip install -e.'
 """
 
 import setuptools
@@ -9,7 +9,7 @@ setuptools.setup(
     name="pydrake",
     version="nightly",
     description="pydrake",
-    url="https://drake.mit.edu/i",
+    url="https://drake.mit.edu/",
     packages=setuptools.find_packages(),
     python_requires=">=3.6"
 )
