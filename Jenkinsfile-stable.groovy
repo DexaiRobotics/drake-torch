35ff22657a687bbe46d13c8ad70b19da9fd01641
Duan Yutong <duan.yutong@dexai.com>
2020-06-11 23:50:56 +0000

    use separate groovy scripts for nightly and stable pipelines to be scheduled on different cadance

---
+++ Jenkinsfile-stable.groovy
@@ -0,0 +1,57 @@
+pipeline {
+    environment {
+        registry = "dexai2/drake-torch"
+        registryCredential = 'dockerhub'
+    }
+    agent any
+    options {
+        parallelsAlwaysFailFast()
+    }
+    stages {
+
+        stage('build_images') {
+            parallel {
+                stage('build_cuda') {
+                    steps {
+                        echo "starting in $PWD"
+                        sh "cd $WORKSPACE"
+                        sh "ls"
+                        sh "./build.sh --cuda --stable"
+                    }
+                }
+                stage('build_cpu') {
+                    steps {
+                        echo "starting in $PWD"
+                        sh "cd $WORKSPACE"
+                        sh "ls"
+                        sh "./build.sh --cpu --stable"
+                    }
+                }
+            }
+        }
+
+        stage('test_dockers') {
+            steps {
+                echo "starting in $PWD"
+                sh "cd $WORKSPACE"
+                sh "ls"
+                sh "./build_test_dockers.sh"
+                sh "./run_docker_tests.sh"
+                // sh "./publish_docker_images.sh" // do not publish by default
+            }
+        }
+
+    }
+
+    post { 
+        always { 
+            // step([$class: 'WsCleanup'])
+            sh "chmod -R 777 ."
+            cleanWs()
+            deleteDir()
+        }
+        cleanup {
+            deleteDir()
+        }
+    }
+}
