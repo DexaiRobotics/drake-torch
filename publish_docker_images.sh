43300cdbea3bf90e373b8bc15d915b5a868d4169
Duan Yutong <duan.yutong@dexai.com>
2020-08-03 16:54:14 -0400

    variable syntax

--- publish_docker_images.sh
+++ publish_docker_images.sh
@@ -7,18 +7,18 @@
 # dexai2/drake-torch:cuda -> dexai2/drake-torch:cuda_YYMMDD
 #                         -> dexai2/drake-torch:cuda_latest
 
-repo_str=dexai2/drake-torch
-suffix_date=$(date +"%Y%m%d")
+REPO_STR=dexai2/drake-torch
+SUFFIX_DATE=$(date +"%Y%m%d")
 
 tag_and_push() {
     BUILD_TYPE=$1
     SUFFIX=$2
     echo "tagging and pushing image, build type $BUILD_TYPE, suffix $SUFFIX"
-    docker tag repo_str:$BUILD_TYPE repo_str:"${BUILD_TYPE}_${SUFFIX}"
+    docker tag $REPO_STR:$BUILD_TYPE $REPO_STR:"${BUILD_TYPE}_${SUFFIX}"
 }
 
 
-tag_and_push cpu date_string
+tag_and_push cpu $SUFFIX_DATE
 tag_and_push cpu latest
-tag_and_push cuda date_string
+tag_and_push cuda $SUFFIX_DATE
 tag_and_push cuda latest
