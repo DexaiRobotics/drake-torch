e08aa3e5039d4c87d5009797726bcf9c765f2649
Duan Yutong <duan.yutong@dexai.com>
2020-09-16 10:29:48 -0400

    colour specifier

--- scripts/fix_bashrc.sh
+++ scripts/fix_bashrc.sh
@@ -51,6 +51,6 @@ parse_git_branch() {
 if [[ ! -v DEPLOYMENT_DOCKER ]]; then 
 	export PS1="\[\033[32m\]\h🐳 \[\033[36m\]\u@dev\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\$(parse_git_branch) $ "
 else
-	export PS1="\[\e[0;49;91m\]\h🐳 \[\033[36m\]\u@deploy\[\033[m\]:\[\033[33;1m\]\w\[\033[m\] $ "
+	export PS1="\[\e[0;49;91m\]\h🐳 \[\033[36m\]\u@\e[0;49;91m\]deploy\[\033[m\]:\[\033[33;1m\]\w\[\033[m\] $ "
 fi
 EOF
