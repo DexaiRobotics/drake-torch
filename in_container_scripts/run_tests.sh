e1069057054056fc603308fd9ad127fa33a7e294
Duan Yutong <duan.yutong@dexai.com>
2020-10-05 15:35:46 -0400

    exit top-level shell not subshell of ()

--- in_container_scripts/run_tests.sh
+++ in_container_scripts/run_tests.sh
@@ -6,18 +6,22 @@ set -eufo pipefail
 if pip list 2>/dev/null | grep -q pydrake &>/dev/null; then
     echo "pydrake not found in pip list"
     exit 1
-else
-    python3 -c "import pydrake" || (echo "pydrake import failure" && exit 1)
+elif python3 -c "import pydrake"; then
     echo "pydrake import test passed"
+else
+    echo "pydrake import failure"
+    exit 1
 fi
 
 # test ROS vision_opencv install
-source ~/.bashrc
-python3 -c "import cv_bridge" || (echo "cv_bridge import failure" && exit 1)
-if python3 -c "import cv_bridge" 2>&1 > /dev/null| grep endian; then
+if ! python3 -c "import cv_bridge"; then
+    echo "cv_bridge import failure"
+    exit 1
+elif python3 -c "import cv_bridge" 2>&1 > /dev/null| grep endian; then
     echo "CV bridge endianness problem found."
     exit 1
+else
+    echo "cv_bridge import test passed with no endianness problem"
 fi
-echo "cv_bridge import test passed with no endianness problem"
 
 /root/scripts/test.py
