156a0b1e8a89dd4db1429b48b0e932935741f191
Duan Yutong <duan.yutong@dexai.com>
2020-10-05 15:38:16 -0400

    stream to stderr

--- in_container_scripts/run_tests.sh
+++ in_container_scripts/run_tests.sh
@@ -4,21 +4,21 @@ set -eufo pipefail
 
 # test pydrake install
 if pip list 2>/dev/null | grep -q pydrake &>/dev/null; then
-    echo "pydrake not found in pip list"
+    echo "pydrake not found in pip list" >&2
     exit 1
 elif python3 -c "import pydrake"; then
     echo "pydrake import test passed"
 else
-    echo "pydrake import failure"
+    echo "pydrake import failure" >&2
     exit 1
 fi
 
 # test ROS vision_opencv install
 if ! python3 -c "import cv_bridge"; then
-    echo "cv_bridge import failure"
+    echo "cv_bridge import failure" >&2
     exit 1
 elif python3 -c "import cv_bridge" 2>&1 > /dev/null| grep endian; then
-    echo "CV bridge endianness problem found."
+    echo "CV bridge endianness problem found." >&2
     exit 1
 else
     echo "cv_bridge import test passed with no endianness problem"
