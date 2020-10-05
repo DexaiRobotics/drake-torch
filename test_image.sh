5eff01c5466bd27cf5463b04915b029ae60e339c
Duan Yutong <duan.yutong@dexai.com>
2020-10-05 11:43:31 -0400

    more image tests

--- test_image.sh
+++ test_image.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # image ID/tag is the required positional argument
 
-docker run -v "$(pwd)/in_container_scripts:/root/scripts" $1 "/root/scripts/test_image.py"
+docker run -v "$(pwd)/in_container_scripts:/root/scripts" $1 "/root/scripts/test_image.sh"
