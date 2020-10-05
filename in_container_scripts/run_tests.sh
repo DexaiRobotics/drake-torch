609a83d5384bd5c87fa399fd96c5a19c5ea612ef
Duan Yutong <duan.yutong@dexai.com>
2020-10-05 11:54:00 -0400

    bug fixes

--- in_container_scripts/run_tests.sh
+++ in_container_scripts/run_tests.sh
@@ -1,19 +1,22 @@
-#!bin/bash
+#!/bin/bash
+
+set -eufo pipefail
 
 # test pydrake install
 if pip list 2>/dev/null | grep -q pydrake &>/dev/null; then
-echo "pydrake not found in pip list"
-exit 1
+    echo "pydrake not found in pip list"
+    exit 1
 else
-python3 -c "import pydrake" || (echo "pydrake import failure" && exit 1)
-echo "pydrake import test passed"
+    python3 -c "import pydrake" || (echo "pydrake import failure" && exit 1)
+    echo "pydrake import test passed"
 fi
 
 # test ROS vision_opencv install
+source ~/.bashrc
 python3 -c "import cv_bridge" || (echo "cv_bridge import failure" && exit 1)
 if python3 -c "import cv_bridge" 2>&1 > /dev/null| grep endian; then
-echo "CV bridge endianness problem found."
-exit 1
+    echo "CV bridge endianness problem found."
+    exit 1
 fi
 echo "cv_bridge import test passed with no endianness problem"
 
--- test_image.sh
+++ test_image.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # image ID/tag is the required positional argument
 
-docker run -v "$(pwd)/in_container_scripts:/root/scripts" $1 "/root/scripts/test_image.sh"
+docker run -v "$(pwd)/in_container_scripts:/root/scripts" $1 bash -ic "/root/scripts/run_tests.sh"
