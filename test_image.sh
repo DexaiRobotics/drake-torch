7d9f13e89920858c8368c1e21a82dfcefdb65dff
Duan Yutong <duan.yutong@dexai.com>
2020-10-05 13:26:01 -0400

    white spaces

--- test_image.sh
+++ test_image.sh
@@ -2,6 +2,6 @@
 # image ID/tag is the required positional argument
 
 docker run \
-    -itv "$(pwd)/in_container_scripts:/root/scripts"\
-    $1\
+    -itv "$(pwd)/in_container_scripts:/root/scripts" \
+    $1 \
     bash -ic "/root/scripts/run_tests.sh"
