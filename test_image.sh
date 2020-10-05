ceba43579648a0bcb534505397ef9ef51f1c445c
Duan Yutong <duan.yutong@dexai.com>
2020-10-05 11:57:35 -0400

    more docker flags to suppress warning

--- test_image.sh
+++ test_image.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 # image ID/tag is the required positional argument
 
-docker run -v "$(pwd)/in_container_scripts:/root/scripts" $1 bash -ic "/root/scripts/run_tests.sh"
+docker run \
+    -itv "$(pwd)/in_container_scripts:/root/scripts"\
+    $1\
+    bash -ic "/root/scripts/run_tests.sh"
