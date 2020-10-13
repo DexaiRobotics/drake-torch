27f78739767c95214ed466d61b3fb994937568f4
Duan Yutong <duan.yutong@dexai.com>
2020-10-13 13:08:54 -0400

    more cv_bridge tests

--- in_container_scripts/run_tests.sh
+++ in_container_scripts/run_tests.sh
@@ -20,8 +20,11 @@ if ! python3 -c "import cv_bridge"; then
 elif python3 -c "import cv_bridge" 2>&1 > /dev/null | grep endian; then
     echo "CV bridge endianness problem found." >&2
     exit 1
+elif ! python3 -c "from cv_bridge.boost.cv_bridge_boost import getCvType"; then
+    echo "dynamic module does not define module export function (PyInit_cv_bridge_boost)" >&2
+    exit 1
 else
-    echo "cv_bridge import test passed with no endianness problem"
+    echo "cv_bridge import tests passed"
 fi
 
 /root/scripts/test.py
