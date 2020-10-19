f928f7ee32a01e7a7d01010e7f4ad75ca17139d5
Duan Yutong <duan.yutong@dexai.com>
2020-10-19 15:02:57 -0400

    reset pythonpath after ros creates duplicates and puts py27 in front

--- in_container_scripts/mod_bashrc.sh
+++ in_container_scripts/mod_bashrc.sh
@@ -18,17 +18,19 @@ sed -i -e 's/^'"$ORIGINAL"'$/'"$REPLACEMENT"'/' "${FIX_FILES[@]}"
 
 cat <<'EOF' >> /root/.bashrc
 
-# prioritise python3 imports, then python2, apt installs to python2
-export PYTHONPATH=/opt/ros/melodic/lib/python3/dist-packages/:$PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:/opt/ros/melodic/lib/python2.7/dist-packages/
-
-if [[ -f /opt/ros/$ROS_DISTRO/setup.bash ]]; then
-    echo "found /opt/ros/$ROS_DISTRO/setup.bash. sourcing..."
-    source /opt/ros/$ROS_DISTRO/setup.bash
+if [[ -f /opt/ros/${ROS_DISTRO}/setup.bash ]]; then
+  echo "found /opt/ros/${ROS_DISTRO}/setup.bash. sourcing..."
+  source /opt/ros/${ROS_DISTRO}/setup.bash
 fi
 if [[ -f $HOME/catkin_ws/devel/setup.bash ]]; then
-    echo "found $HOME/catkin_ws/devel/setup.bash. sourcing..."
-    source $HOME/catkin_ws/devel/setup.bash
+  echo "found $HOME/catkin_ws/devel/setup.bash. sourcing..."
+  source $HOME/catkin_ws/devel/setup.bash
+fi
+
+# prioritise python3 imports, then python2, apt installs to python2
+if [[ $(lsb_release -sc) == "bionic" ]]; then
+  export PYTHONPATH=/opt/ros/melodic/lib/python3/dist-packages/
+  export PYTHONPATH=$PYTHONPATH:/opt/ros/melodic/lib/python2.7/dist-packages/
 fi
 export ROS_PYTHON_VERSION=3
 
@@ -37,9 +39,9 @@ parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 if [[ ! -v DEPLOYMENT_DOCKER ]]; then 
-	export PS1="\[\033[32m\]\h🐳 \[\033[36m\]\u@dev\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\$(parse_git_branch) $ "
+  export PS1="\[\033[32m\]\h🐳 \[\033[36m\]\u@dev\[\033[m\]:\[\033[33;1m\]\w\[\033[m\]\$(parse_git_branch) $ "
 else
-	export PS1="\[\e[0;49;91m\]\h🐳 \[\033[36m\]\u@\e[0;49;91m\]deploy\[\033[m\]:\[\033[33;1m\]\w\[\033[m\] $ "
+  export PS1="\[\e[0;49;91m\]\h🐳 \[\033[36m\]\u@\e[0;49;91m\]deploy\[\033[m\]:\[\033[33;1m\]\w\[\033[m\] $ "
 fi
 
 EOF
